87bcc906e78f3c38e8a89132af4965e1d5e88a27
FELIPE <FELIPE@MendiPati>
2018-02-01 23:19:24 -0500

    05/12/2016 We first class created. Initial project. 05/12/2016 18:00hrs Wallen's work (Classes) 05/12/2016 19:20hrs Felipe's work (Classes) 05/14/2016 15:20hrs Felipe tests classes

--- CIT260OregonTrail/src/model/CheckPoints.java
+++ CIT260OregonTrail/src/model/CheckPoints.java
@@ -5,10 +5,84 @@
  */
 package model;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
  * @author user
  */
-public class CheckPoints {
+public class CheckPoints implements Serializable{
+
+    private String name;
+    private int coordinate;
+    private String visited; 
     
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 29 * hash + Objects.hashCode(this.name);
+        hash = 29 * hash + this.coordinate;
+        hash = 29 * hash + Objects.hashCode(this.visited);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CheckPoints other = (CheckPoints) obj;
+        if (this.coordinate != other.coordinate) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.visited, other.visited)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "CheckPoints{" + "name=" + name + ", coordinate=" + coordinate + ", visited=" + visited + '}';
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(int coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    public String getVisited() {
+        return visited;
+    }
+
+    public void setVisited(String visited) {
+        this.visited = visited;
+    }
+    
+    
+
+ public CheckPoints() {
+    }
 }
+
