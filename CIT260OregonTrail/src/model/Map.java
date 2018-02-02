d2436ce778f4287be82927126be48b8dea69f82d
bould <bould@LAPTOP-5KTL16IG>
2018-02-01 20:33:32 -0700

    Map

--- CIT260OregonTrail/src/model/Map.java
+++ CIT260OregonTrail/src/model/Map.java
@@ -5,10 +5,70 @@
  */
 package model;
 
+import java.io.Serializable;
+
 /**
  *
  * @author user
  */
-public class Map {
+public class Map implements Serializable{
+    private int rowCount;
+    private int columnCount;
+
+    public Map() {
+    }
+    
+    
+
+    public int getRowCount() {
+        return rowCount;
+    }
+
+    public void setRowCount(int rowCount) {
+        this.rowCount = rowCount;
+    }
+
+    public int getColumnCount() {
+        return columnCount;
+    }
+
+    public void setColumnCount(int columnCount) {
+        this.columnCount = columnCount;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + this.rowCount;
+        hash = 37 * hash + this.columnCount;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Map other = (Map) obj;
+        if (this.rowCount != other.rowCount) {
+            return false;
+        }
+        if (this.columnCount != other.columnCount) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Map{" + "rowCount=" + rowCount + ", columnCount=" + columnCount + '}';
+    }
+    
     
 }
