cc1e68cb1b304f3b6f9170f1692c3972c648dfe5
user <user@user-PC>
2018-02-14 23:23:28 -0500

    02/14/2018 Individual Assignment

--- CIT260OregonTrail/test/control/ItemControlTest.java
+++ CIT260OregonTrail/test/control/ItemControlTest.java
@@ -5,6 +5,7 @@
  */
 package control;
 
+import model.Items;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -29,8 +30,53 @@ public class ItemControlTest {
         boolean expResult = true;
         boolean result = instance.addItemToStock(name, quantity);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("Por que falla loco?");
+    }
+
+    /**
+     * Test of useResource method, of class ItemControl.
+     */
+    @Test
+    public void testUseResource() {
+        System.out.println("useResource");
+        System.out.println("----------------------------------");
+        System.out.println("Test 1");
+        Items item = new Items();
+        item.setName("Oxen");
+        int quantity = 3;
+        ItemControl instance = new ItemControl();
+        boolean expResult = true;
+        boolean result = instance.useResource(item, quantity);
+        assertEquals(expResult, result);
+        
+        System.out.println("----------------------------------");
+        System.out.println("Test 2");
+        item = new Items();
+        item.setName("Wheel");
+        quantity = 0;
+        instance = new ItemControl();
+        expResult = false;
+        result = instance.useResource(item, quantity);
+        assertEquals(expResult, result);
+        
+        System.out.println("----------------------------------");
+        System.out.println("Test 3");
+        item = new Items();
+        item.setName("Clothe");
+        quantity = -2;
+        instance = new ItemControl();
+        expResult = false;
+        result = instance.useResource(item, quantity);
+        assertEquals(expResult, result);
+        
+        System.out.println("----------------------------------");
+        System.out.println("Test 4");
+        item = new Items();
+        item.setName("Food");
+        quantity = 1;
+        instance = new ItemControl();
+        expResult = true;
+        result = instance.useResource(item, quantity);
+        assertEquals(expResult, result);
     }
     
 }
---
+++ CIT260OregonTrail/test/control/MapControlTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import model.CheckPoints;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author user
+ */
+public class MapControlTest {
+    
+    public MapControlTest() {
+    }
+
+    /**
+     * Test of moveToNewLocation method, of class MapControl.
+     */
+    @Test
+    public void testMoveToNewLocation() {
+        System.out.println("moveToNewLocation");
+        System.out.println("----------------------------------");
+        System.out.println("Test 1");
+        CheckPoints checkPoint = new CheckPoints();
+        checkPoint.setVisited(true);
+        MapControl instance = new MapControl();
+        boolean expResult = false;
+        boolean result = instance.moveToNewLocation(checkPoint);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+        System.out.println("----------------------------------");
+        System.out.println("Test 2");
+        checkPoint.setVisited(false);
+        expResult = true;
+        result = instance.moveToNewLocation(checkPoint);
+        assertEquals(expResult, result);
+    }
+    
+}
