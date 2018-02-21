0a546515111fb389f6b1e8f74f7b92dcf68e961f
user <user@user-PC>
2018-02-21 00:23:37 -0500

    02/20/2018

--- CIT260OregonTrail/src/cit260oregontrail/CIT260OregonTrail.java
+++ CIT260OregonTrail/src/cit260oregontrail/CIT260OregonTrail.java
@@ -5,13 +5,8 @@
  */
 package cit260oregontrail;
 
-import model.Game;
-import model.Items;
-import model.Player;
-import model.WagonPartyMembers;
-import model.Map;
-import model.Occupation;
-import model.CheckPoints;
+import view.StartProgramView;
+
 
 /**
  *
@@ -20,56 +15,8 @@ import model.CheckPoints;
 public class CIT260OregonTrail {
 
     public static void main(String[] args) {
-        
-        Game game = new Game();
-        Player player = new Player();
-        Items items = new Items();
-        WagonPartyMembers wagonPartyMembers = new WagonPartyMembers();
-        Map map = new Map();
-        Occupation occupation = new Occupation();
-        CheckPoints checkPoints = new CheckPoints();
-        
-        
-        player.setName("Ingrid");
-        player.setBetScore(50000000);
-        
-        game.setFoodRations(3);
-        game.setNumberOfPeople(5);
-        game.setPace(1);
-        game.setPlayer(player);
-        
-        items.setCost(60);
-        items.setName("Oxen");
-        items.setGame(game);
-        items.setQuantity(2);
-        
-        wagonPartyMembers.setGame(game);
-        wagonPartyMembers.setHealth("Good");
-        wagonPartyMembers.setName("Name of Test");
-        
-        map.setRowCount (15);         
-        map.setColumnCount (25);
-        
-        
-        occupation.setName ("Carpenter");
-        occupation.setMoney (800);
-        occupation.setDescription("Wood Worker");
-        
-        
-        checkPoints.setName ("Fort Sumter");         
-        checkPoints.setCoordinate (25);         
-        checkPoints.setVisited(true);
-        
-        
-        
-        System.out.println(game.toString() 
-                         + "\n" + player.toString()
-                         + "\n" + items.toString()
-                         + "\n" + wagonPartyMembers.toString()
-                         + "\n" + map.toString()
-                         + "\n" + occupation.toString()
-                         + "\n" + checkPoints.toString());
-        
+        StartProgramView startProgramView = new StartProgramView();
+        startProgramView.display();
     }
     
 }
---
+++ CIT260OregonTrail/src/view/StartProgramView.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Govert Carreño
+ */
+public class StartProgramView {
+
+    public StartProgramView() {
+    }
+    
+    public void display(){
+//        displayStartProgramView() {
+        // endOfView = false
+        // DO
+        //inputs = getInputs()
+        //IF no inputs were entered OR the first input is Q
+        // RETURN
+        //ENDIF
+        //endOfView = doAction(inputs)
+        //WHILE endOfView != true
+        //}
+       
+        boolean endOfView = false;
+        do{
+            String[] inputs = this.getInputs();
+            if(inputs.length == 0 || "Q".equals(inputs[0])) 
+                return;
+
+            endOfView = doAction(inputs);
+        }while(!endOfView);
+       
+       
+       
+    }
+
+    private String[] getInputs() {
+//    getInputs(): String[] 
+//
+//        inputs = new String array one element long
+//        Display a description of the view
+//
+//        valid = false
+//        WHILE valid == false (no input value has been enterd)
+//
+//            Display the prompt message
+//            Get the value entered from the keyboard
+//            Trim off leading and trailing blanks from the value
+//            IF length of the value < 1 then
+//                Display "You must enter a non-blank value”
+//                Continue (move to the top of the loop and repeat)
+//            ENDIF
+//            Assign the value to the first position in the inputs array
+//            valid = true (ends the loop)
+//        ENDWHILE
+//
+//    RETURN inputs 
+        
+        String[] inputs = new String[1];
+        System.out.println("Esta parte es de Aaron");
+        
+        boolean valid = false;
+        
+        while(!valid){
+            System.out.println("Please enter the player's name");
+            Scanner sc = new Scanner(System.in);
+            String value = sc.nextLine();
+            value = value.trim();
+            
+            if(value.length() < 1){
+                System.out.println("You must enter a non-blank value");
+                continue;
+            }
+            
+            inputs[0] = value.toUpperCase();
+            valid = true;
+        }
+        
+        return inputs;
+    }
+
+    private boolean doAction(String[] inputs) {
+//        doAction(inputs): boolean {
+//            playersName = get the first value in the inputs array
+//            player = savePlayer(playersName)
+//        IF player == null
+//            display “Could not create the player. “ +
+//            “Enter a different name.”
+//            RETURN false
+//        ENDIF
+//        DISPLAY "================================================= "
+//        + "Welcome to the game " + playersName
+//        + "We hope you have a lot of fun!”
+//        + "================================================= "
+//        mainMenuView = Create a new MainMenuView object
+//        mainMenuView.displayMainMenuView()
+//        RETURN true
+//        }
+        
+        return true;
+    }
+}
