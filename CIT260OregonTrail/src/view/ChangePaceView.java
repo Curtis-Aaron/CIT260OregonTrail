43971deb33333ddc477001df98e2cfb8f8cb4c20
bould <bould@LAPTOP-5KTL16IG>
2018-02-28 16:42:30 -0700

    02/28/2018

--- CIT260OregonTrail/src/view/ChangePaceView.java
+++ CIT260OregonTrail/src/view/ChangePaceView.java
@@ -63,5 +63,17 @@ public class ChangePaceView {
 
     private boolean doAction(String[] inputs) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//        I already wrote this when I realized I wouldnt finish this view.
+//                "\nSteady-You travel about 8 hours a day, taking frequent rests."
+//        + "\nYou take care not to get too tired."
+//        + "\n"
+//        + "\nStrenuous-You travel about 12 hours a day, starting just after"
+//        + "\nsunrise and stopping shortly before sunset. You stop to rest"
+//        + "\nonly when necessary. You finish each day feeling ver tired."
+//        + "\n"
+//        + "\nGrueling-You travel about 16 hours a day, starting before sunrise"
+//        + "\nand continuing until dark. You almost never stop to rest. You do"
+//        + "\nnot get enough sleep at night. You finish each day feeling abosolutely"
+//        + "\nexhausted, and your health suffers."
     }
 }
