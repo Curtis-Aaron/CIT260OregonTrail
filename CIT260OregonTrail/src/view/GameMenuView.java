909ebd57bbd055e3a18a371b462f1dfad77c4880
bould <bould@LAPTOP-5KTL16IG>
2018-02-28 00:26:06 -0700

    02/27/2018

--- CIT260OregonTrail/src/view/GameMenuView.java
+++ CIT260OregonTrail/src/view/GameMenuView.java
@@ -36,14 +36,96 @@ public class GameMenuView {
     }
 
     private String[] getInputs() {
-        System.out.println("get inputs gameMenuView Run");
-        String[] prueba = new String[1];
-        prueba[0] = "algo";
-        return prueba;
+       String[] inputs = new String [1];
+       boolean valid = false;
+        
+       while (!valid){
+            System.out.println("\n\n Pick a Menu OPTION"
+                        + "\n1. Continue on trail"
+                        + "\n2. Check Supplies"
+                        + "\n3. Look at Map" 
+                        + "\n4. Change Pace"
+                        + "\n5. Food Rations"
+                        + "\n6. Stop to Rest"
+                        + "\n7. Attempt to trade"
+                        + "\n8. Hunt for food");
+
+            java.util.Scanner sc = new java.util.Scanner(System.in);
+            String value = sc.nextLine();
+            value = value.trim();
+
+            if(value.length() < 1){
+                System.out.println("Please select an option.");
+                continue;
+            }
+            inputs[0] = value;
+            valid = true;
+       }
+        return inputs;
     }
 
     private boolean doAction(String[] inputs) {
-        System.out.println("doAction gameMenuView Run");
-        return true;
+        String menuItem = inputs[0].toUpperCase();
+        switch (menuItem) {
+            case "1":  
+                this.continueOnTrail();
+                break;
+            case "2":  
+                this.checkSupplies();
+                break;
+            case "3":  
+                this.lookAtMap();
+                break;
+            case "4":  
+                this.changePace();
+                break;
+            case "5":  
+                this.foodRations();
+                break;
+            case "6":  
+                this.stopToRest();
+                break;
+            case "7":  
+                this.attemptToTrade();
+                break;
+            case "8":  
+                this.huntForFood();
+                break;
+            default: System.out.println("Invalid menu item.");
+                     break;
+    }
+        return false;
+    }
+
+    private void continueOnTrail() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void checkSupplies() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void lookAtMap() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void changePace() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void foodRations() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void stopToRest() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void attemptToTrade() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void huntForFood() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
