5fbdc19be89d626acfcb808840c340a24e81316e
user <user@user-PC>
2018-03-03 11:00:35 -0500

    03-03-2018

--- CIT260OregonTrail/src/view/QuietView.java
+++ CIT260OregonTrail/src/view/QuietView.java
@@ -14,7 +14,7 @@ public class QuietView {
     public QuietView(){
         
     }
-      public void display(){
+    public void display(){
 //            display() {
 //            endView = false
 //           DO
@@ -33,66 +33,67 @@ public class QuietView {
                 endOfView = doAction(inputs);
             }
             while(!endOfView);
-    
-  public boolean doAction(String[] inputs) {
-        
-//                doAction(inputs): boolean {
-//                  menuItem = first element in inputs array
-//                  convert menuItem to upper case
-//                  SWITCH menuItem
-//                      “N”: startNewGame()
-//                      “R”: restartGame()
-//                      “H”: getHelp()
-//                      “E”: RETURN true
-//                      DEFAULT: DISPLAY “Invalid menu item.”
-//                   ENDSWITCH
-//                   RETURN false
-//                }
-//  MainMenuView mainMenuView = new MainMenuView();
-//        mainMenuView.display();
-//              System.out.println("**** MainMenuView goAction method Run ***");
-//              return true;
-
-        String menuItem = inputs[0].toUpperCase();
-        switch (menuItem) {
-            case "1":  
-                this.startNewGame();
-                break;
-            case "2":  
-                this.restartGame();
-                break;
-            case "3":  
-                this.getHelp();
-                break;
-            default: System.out.println("Invalid menu item.");
-                     break;
     }
-        return false;
-}
+        public boolean doAction(String[] inputs) {
+
+      //                doAction(inputs): boolean {
+      //                  menuItem = first element in inputs array
+      //                  convert menuItem to upper case
+      //                  SWITCH menuItem
+      //                      “N”: startNewGame()
+      //                      “R”: restartGame()
+      //                      “H”: getHelp()
+      //                      “E”: RETURN true
+      //                      DEFAULT: DISPLAY “Invalid menu item.”
+      //                   ENDSWITCH
+      //                   RETURN false
+      //                }
+      //  MainMenuView mainMenuView = new MainMenuView();
+      //        mainMenuView.display();
+      //              System.out.println("**** MainMenuView goAction method Run ***");
+      //              return true;
+
+              String menuItem = inputs[0].toUpperCase();
+              switch (menuItem) {
+                  case "1":  
+                      //this.startNewGame();
+                      break;
+                  case "2":  
+                      //this.restartGame();
+                      break;
+                  case "3":  
+                      //this.getHelp();
+                      break;
+                  default: System.out.println("Invalid menu item.");
+                           break;
+          }
+              return false;
+      }
     
-}
-private string [] getInputs (){
-    String[] inputs = new String[1];
-    boolean valid = false;
 
-    while (!valid){
-         System.out.println ("\n\n The Oregon Trail"
-                             + "\n1. 1. Save and Quite"
-                             + "\n2. 2. Quit"
-                             + "\n3. C to continue"
-                             + "\nQ. What is your choice?");
+    private String[] getInputs (){
+        String[] inputs = new String[1];
+        boolean valid = false;
 
-         java.util.Scanner sc = new java.util.Scanner(System.in);
-         String value = sc.nextLine();
-         value = value.trim();
-            
-            if(value.length() < 1){
-                System.out.println("You have to choose");
-                continue;
-            }
-            inputs[0] = value;
-            valid = true;
-        }   
+        while (!valid){
+             System.out.println ("\n\n The Oregon Trail"
+                                 + "\n1. 1. Save and Quite"
+                                 + "\n2. 2. Quit"
+                                 + "\n3. Q. Cancel "
+                                 + "\nQ. What is your choice?");
 
-        return inputs;
+             java.util.Scanner sc = new java.util.Scanner(System.in);
+             String value = sc.nextLine();
+             value = value.trim();
+
+                if(value.length() < 1){
+                    System.out.println("You have to choose");
+                    continue;
+                }
+                inputs[0] = value;
+                valid = true;
+            }   
+
+            return inputs;
+    }
 }
