c63ebf654373527d1fa51d1a595bd825038a6e6d
user <user@user-PC>
2018-03-21 00:29:53 -0500

    03-20-2018

--- CIT260OregonTrail/src/view/MattsGeneralStoreView.java
+++ CIT260OregonTrail/src/view/MattsGeneralStoreView.java
@@ -5,6 +5,7 @@
  */
 package view;
 
+import cit260oregontrail.CIT260OregonTrail;
 import java.util.ArrayList;
 import model.Items;
 
@@ -13,58 +14,108 @@ import model.Items;
  * @author Govert Carreño
  */
 public class MattsGeneralStoreView extends Views{
-    private String menu;
-    private ArrayList<Items> itemsList = null;
+    private String showMenu;
     
-    float totOxen = 0;
-    float totFoot = 0;
-    float totCloth = 0;
-    float totAmmunition = 0;
+    private float cOxen = 0;
+    private float cFood = 0;
+    private float cClothing = 0;
+    private float cAmmunition = 0;
+    private float cWWheel = 0;
+    private float cWAxle = 0;
+    private float cWTongue = 0;
 
     public MattsGeneralStoreView() {
-        //super("\n******** The Oregon Trail – Matt’s General Store  ********"
-        //    + "\n*     Choose an item to add to your order:               *"
-//            + "\n*     1. Oxen                              " + totOxen  + "           *"
-//            + "\n*     2. Food                              " + totFoot  + "           *"
-//            + "\n*     3. Clothing                          " + totCloth + "           *"
-//            + "\n*     4. Ammunition                        " + totAmmunition + "           *"
-//            + "\n*     5. Spare Parts                       $0            *"
-//            + "\n*               Funds Remaining            $" + totAmmunition + "         *"
-//            + "\n*     What is your choice (C to continue)?               *"
-//            + "\n**********************************************************");
+        displayMessage = "\n******** The Oregon Trail – Matt’s General Store  ********"
+            + "\n*     Choose an item to add to your order:               *"
+            + "\n*     1. Oxen                              " + cOxen  + "           *"
+            + "\n*     2. Food                              " + cFood  + "           *"
+            + "\n*     3. Clothing                          " + cClothing + "           *"
+            + "\n*     4. Ammunition                        " + cAmmunition + "           *"
+            + "\n*     ** Spare Parts                       $            *"
+            + "\n*        5. Wagon Wheel                       " + cWWheel + "           *"
+            + "\n*        6. Wagon Axle                        " + cWAxle + "           *"
+            + "\n*        7. Wagon Tongue                      " + cWTongue + "           *"
+            + "\n*               Funds Remaining            $" + CIT260OregonTrail.getGame().getOccupation().getMoney() + "         *"
+            + "\n*     What is your choice (C to continue)?               *"
+            + "\n**********************************************************";
+        
     }
             
     @Override
     public boolean doAction(String inputs) {
         
-        String item = inputs.toUpperCase();
+        String selection = inputs.toUpperCase();
+        selection = selection.trim();
+        int numSel = Character.getNumericValue(selection.charAt(0));
         
-        switch (item){
-            case "1": 
-                    /*this.frequentlyQuestions();*/
-                    break;
-            case "2": 
-                    /*this.aboutTheGame();*/
-                    break;
-            case "3": 
-                    /*this.generalInformation();*/
-                    break;
-            case "4": 
-                    /*this.whenToBuy();*/
-                    break;
-            case "Q": 
-                return true;
-            default:
-                return true;
+        switch(selection){
+            case "1":
+                this.promtQuantity(ItemList.OXEN.ordinal());
+                break;
+            case "2":
+                this.promtQuantity(ItemList.FOOD.ordinal());
+                break;
+            case "3":
+                this.promtQuantity(ItemList.CLOTH.ordinal());
+                break;
+            case "4":
+                this.promtQuantity(ItemList.AMMUNITION.ordinal());
+                break;
+            case "5":
+                this.promtQuantity(ItemList.WAGON_WHEEL.ordinal());
+                break;    
+            case "6":
+                this.promtQuantity(ItemList.WAGON_AXLE.ordinal());
+                break;
+            case "7":
+                this.promtQuantity(ItemList.WAGON_TONGUE.ordinal());
+                break;
+            case "C":
+                this.displayNextView();
+                break;
+                
+                
         }
-
+ 
         return true;
     }
-
+    private void updateMenu(){
+        showMenu = "\n******** The Oregon Trail – Matt’s General Store  ********"
+            + "\n*     Choose an item to add to your order:               *"
+            + "\n*     1. Oxen                              " + cOxen  + "           *"
+            + "\n*     2. Food                              " + cFood  + "           *"
+            + "\n*     3. Clothing                          " + cClothing + "           *"
+            + "\n*     4. Ammunition                        " + cAmmunition + "           *"
+            + "\n*     ** Spare Parts                       $            *"
+            + "\n*        5. Wagon Wheel                       " + cWWheel + "           *"
+            + "\n*        6. Wagon Axle                        " + cWAxle + "           *"
+            + "\n*        7. Wagon Tongue                      " + cWTongue + "           *"
+            + "\n*               Funds Remaining            $" + CIT260OregonTrail.getGame().getOccupation().getMoney() + "         *"
+            + "\n*     What is your choice (C to continue)?               *"
+            + "\n**********************************************************";
+    }
     
     @Override
     public void displayNextView() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+    }
+
+    private void promtQuantity(int index) {
+        ArrayList<Items> itemsList = CIT260OregonTrail.getGame().getItems();
+        
+        Items selectedItem = itemsList.get(index);
+        this.updateMenu();
+        this.displayMessage = this.showMenu
+                            + "\n\nEach "+ selectedItem.getName() +" has a cost of $" + selectedItem.getCost() + ", "
+                            + "how much do you want to buy?"
+                            + "\nPres Q to return to Matt's General Store.";
+
+        String res = this.getInputs();
+        res = res.toUpperCase();
+        if(res.equals("Q")) {
+            this.displayMessage = this.showMenu;
+            this.display();
+        }
     }
 
     
