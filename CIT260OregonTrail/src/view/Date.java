2e6a94ed389d848f6bcfbff1b54c4876139d62af
ING <>
2018-04-04 21:09:52 -0500

    DATE

---
+++ CIT260OregonTrail/src/view/Date.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+/**
+ *
+ * @author ING
+ */
+public class Date {
+     public ChooseMonthOptionView() {
+        super("\n\n <--------|-------->It is 1848. Your  jumping off  place for Oregon <--------|-------->"
+            + "\n\n <--------|-------->is Independence, Missouri. You must select which<--------|-------->"
+            + "\n\n <--------|-------->         month to leave Independence.           <--------|-------->"
+            + "\n1. >MARCH<"
+            + "\n2. >APRIL<"
+            + "\n3. >MAY<"
+            + "\n4. >JUNE<"
+            + "\n5. >JULY<"
+            + "\nQ. >QUIT<"
+            + "\n\n <-------------|-------------><-------------|------------->");
+    }
+    
+    @Override
+
+    private boolean ChooseMonth(String value, Game currentGame) {
+        switch (value)
+        {
+            case "1": //March
+                this.console.println("The month is March.");
+                //scene.setCurrentMonth("March");
+                this.showMainMenu(currentGame);
+                break;
+            case "2": //April
+                this.console.println("The month is April.");
+                //scene.setCurrentMonth("April");
+                this.showMainMenu(currentGame);
+                break;
+            case "3": //May
+                this.console.println("The month is May.");
+                //scene.setCurrentMonth("May");
+                this.showMainMenu(currentGame);
+                break;
+            case "4": //June
+                this.console.println("The month is June.");
+                //scene.setCurrentMonth("June");
+                this.showMainMenu(currentGame);
+                break;
+            case "5": //July
+                this.console.println("The month is July.");
+                //scene.setCurrentMonth("July");
+                this.showMainMenu(currentGame);
+                break;
+            
+}
+    
+
